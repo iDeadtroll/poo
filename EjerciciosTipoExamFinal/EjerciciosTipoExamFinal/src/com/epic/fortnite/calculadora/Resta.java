178e24252e11053dc57ddbf625815d55248e4cc5
Edwin <mamisho@hotmail.es>
2023-05-22 00:03:15 +0200

    feat(CALCULADORA) - Comparable

--- EjerciciosTipoExamFinal/EjerciciosTipoExamFinal/src/com/epic/fortnite/calculadora/Resta.java
+++ EjerciciosTipoExamFinal/EjerciciosTipoExamFinal/src/com/epic/fortnite/calculadora/Resta.java
@@ -5,11 +5,13 @@
  */
 package com.epic.fortnite.calculadora;
 
+import java.util.Objects;
+
 /**
  *
  * @author Deadtroll
  */
-public class Resta implements IOperacion{
+public class Resta implements IOperacion {
     private String name;
     
     @Override
@@ -21,5 +23,40 @@ public class Resta implements IOperacion{
     @Override
     public Double ejecutar(double val1, double val2){
         return val1 - val2;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Resta other = (Resta) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    @Override
+    public int compareTo(Object o) {
+        IOperacion oper = (IOperacion) o;
+        String nombreExterno = oper.getNombre();
+        if(this.getNombre().length() > nombreExterno.length()) {
+            return 1;
+        }
+        
+        if(this.getNombre().length() == nombreExterno.length()) {
+            return 0;
+        }
+        
+        return -1;
     }
 }
