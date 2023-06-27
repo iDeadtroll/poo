e11dba78d76572a7982564083afd741c79113a04
iDeadtroll <javielproductor@gmail.com>
2023-06-27 05:25:57 +0200

    nada
    nada

--- Pruebas/src/examenPooCorregido/ComparadorAntiguedadAbonado.java
+++ Pruebas/src/examenPooCorregido/ComparadorAntiguedadAbonado.java
@@ -10,22 +10,23 @@ package examenPooCorregido;
  * @author Deadtroll
  */
 import java.util.Comparator;
-public class ComparadorAntiguedadAbonado implements Comparator {
- public int compare(Object o1, Object o2) {
- Abonado a1 = (Abonado) o1;
- Abonado a2 = (Abonado) o2;
- int cmp = 0;
- if (a1.isActivo() && !a2.isActivo()) {
- cmp = -1;
- } else if (!a1.isActivo() && a2.isActivo()) {
- cmp = 1;
- } else {
- cmp = a1.getApellidos().compareToIgnoreCase(a2.getApellidos());
- if (cmp == 0) {
- cmp = a1.getNombre().compareToIgnoreCase(a2.getNombre());
- }
- }
- return cmp;
- }
-}
 
+public class ComparadorAntiguedadAbonado implements Comparator {
+
+    public int compare(Object o1, Object o2) {
+        Abonado a1 = (Abonado) o1;
+        Abonado a2 = (Abonado) o2;
+        int cmp = 0;
+        if (a1.isActivo() && !a2.isActivo()) {
+            cmp = -1;
+        } else if (!a1.isActivo() && a2.isActivo()) {
+            cmp = 1;
+        } else {
+            cmp = a1.getApellidos().compareToIgnoreCase(a2.getApellidos());
+            if (cmp == 0) {
+                cmp = a1.getNombre().compareToIgnoreCase(a2.getNombre());
+            }
+        }
+        return cmp;
+    }
+}
