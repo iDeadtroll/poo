21306b5209ecd5dda084e80dca93ed69b1d4c456
Jonathan Javier Quishpe Maldonado <94075117+iDeadtroll@users.noreply.github.com>
2024-05-06 02:01:45 +0000

    EPDFinal2324: version 1.0

--- Tema4/EPDFinal2324/EPDFinal2324/src/epdfinal2324/Evento.java
+++ Tema4/EPDFinal2324/EPDFinal2324/src/epdfinal2324/Evento.java
@@ -27,19 +27,48 @@ public class Evento {
     }
 
     public void setFecha(String f) {
-    // TODO: comprobar que se cumple el patrón 'dd/mm/aaaa', caso contrario se inicializa a '01/01/1900'
-        this.fecha = f;
+        if (fechaValida(f)) {
+            this.fecha = f;
+        } else {
+            this.fecha = "01/01/1900";
+        }
+    }
+
+    private boolean fechaValida(String fecha) {
+        if (fecha.length() != 10)
+            return false;
+
+        String[] partes = fecha.split("/");
+        if (partes.length != 3)
+            return false;
+
+        String dia = partes[0];
+        String mes = partes[1];
+        String año = partes[2];
+
+        if (dia.length() != 2 || mes.length() != 2 || año.length() != 4)
+            return false;
+
+        int diaNum = Integer.parseInt(dia);
+        int mesNum = Integer.parseInt(mes);
+
+        if (diaNum < 1 || diaNum > 31)
+            return false;
+        if (mesNum < 1 || mesNum > 12)
+            return false;
+
+        return true;
     }
 
     @Override
     public String toString() {
         String cadena = "   -> EVENTO: " + this.nombre + " <" + this.fecha + ">\n";
-        if (contadorAtletas != 0){
-            for (int i = 0; i < contadorAtletas; i++){
-                cadena += "   ----> " + atletas[i].nombre + " ("+ atletas[i].pais +") " +"\n";
+        if (contadorAtletas != 0) {
+            for (int i = 0; i < contadorAtletas; i++) {
+                cadena += "   ----> " + atletas[i].nombre + " (" + atletas[i].pais + ") " + "\n";
             }
-        }else{
-            cadena +="        <Aún no hay atletas registrados en este evento>" + "\n";
+        } else {
+            cadena += "        <Aún no hay atletas registrados en este evento>" + "\n";
         }
         return cadena;
     }
